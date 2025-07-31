c804cbaed1185fb281acc58e22904ae2063c57bd
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-07-31 16:23:43 -0300

    feat: configure env

--- firebase.ts
+++ firebase.ts
@@ -1,15 +1,24 @@
-// firebase.ts
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MEASUREMENT_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET
+} from '@env';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBi-xMV9YcINXEtLpykuPlrsW-hjWurVF0',
-  authDomain: 'agendala-68714.firebaseapp.com',
-  projectId: 'agendala-68714',
-  storageBucket: 'agendala-68714.appspot.com',
-  messagingSenderId: '600695293831',
-  appId: '1:600695293831:web:129d0e445d834fd8837522',
-  measurementId: 'G-PZCF664R7K',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
